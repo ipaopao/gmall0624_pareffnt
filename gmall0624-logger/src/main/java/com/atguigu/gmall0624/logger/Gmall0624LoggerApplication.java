d7d5774f33b95104c0ecf145d639ce2d474535b9
ipaopao <yangzhiguo@360.com>
2021-01-27 12:09:15 +0800

    sssss

--- gmall0624-logger/src/main/java/com/atguigu/gmall0624/logger/Gmall0624LoggerApplication.java
+++ gmall0624-logger/src/main/java/com/atguigu/gmall0624/logger/Gmall0624LoggerApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Gmall0624LoggerApplication {
 
+
+    /*yangzhiguo*/
     public static void main(String[] args) {
         SpringApplication.run(Gmall0624LoggerApplication.class, args);
     }
